7c09f04afd78322d670911d97bd11dbdc8913b56
Hasith435 <66883843+Hasith435@users.noreply.github.com>
2022-08-24 11:37:16 +0400

    fdsafasdf

--- main.py
+++ main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+import numpy as np
+from words import get_wordle_answers, get_wordle_guesses
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+wordle_answers = get_wordle_answers()
+wordle_guesses = get_wordle_guesses()
+
+def req_word_color_code():
+    word_sep = []
+    color_code_sep = []
+
+    word = str(input('word: '))
+    color_code = str(input('color_code: '))
+
+    for letters in word:
+        word_sep.append(letters)
+
+    for code in color_code:
+        color_code_sep.append(code)
+
+    return word_sep, color_code_sep
+
+#(['c', 'r', 'a', 'n', 'e'], ['G', 'G', 'g', 'g', 'Y'])
+req_details = req_word_color_code()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def most_common_word(sep_word, sep_color_code):
+    sep_word = sep_word
+    sep_color_code = sep_color_code
+
+    G_letters = []
+    g_letters = []
+    Y_letters = []
+
+    G_letter_index = []
+    g_letter_index = []
+    Y_letter_index = []
+
+    for i in range(len(sep_color_code)):
+        if sep_color_code[i] == "G":
+            G_letters.append(sep_word[i])
+            G_letter_index.append(i)
+
+        if sep_color_code[i] == "g":
+            g_letters.append(sep_word[i])
+            g_letter_index.append(i)
+
+        if sep_color_code[i] == "Y":
+            Y_letters.append(sep_word[i])
+            Y_letter_index.append(i)
+
+    # print(G_letter_index)
+    # print(g_letter_index)
+    # print(Y_letter_index)
+
+    G_letter_words = []
+    Y_letter_words = []
+
+    count = 0
+    current_word_letters = []
+
+    for word in wordle_answers:
+        for word_letter in word:
+            current_word_letters.append(word_letter)
+
+        for G_letter_indexes in G_letter_index:
+            for G_let in G_letters:
+                if current_word_letters[G_letter_indexes] == G_let:
+                    print("equal")
+                    count += 1
+
+        if count == len(G_letters):
+            G_letter_words.append(word)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(G_letter_words)
+
+print(most_common_word(req_details[0], req_details[1]))
+
+
+
+
