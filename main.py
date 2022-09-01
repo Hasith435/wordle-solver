0ccfaabeaedd593ed5fb5f41b719816f54a75488
Hasith435 <66883843+Hasith435@users.noreply.github.com>
2022-09-01 23:36:23 +0400

    tried to use a different way....where instead of creating a new list and appending the most probable words into it ill just edit the wordle answers list itself by removing the unwanted ones. This makes it easier to work with the list when carrying out multiple instances for different words.

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from words import get_wordle_answers, get_wordle_guesses
 wordle_answers = get_wordle_answers()
 wordle_guesses = get_wordle_guesses()
 
-def req_word_color_code():
+num_program_runtimes = 0
+word_database = 0
 
-    word = input('word: ')
+most_probable_words = []
+
+
+def most_common_word():
+    global word_database, num_program_runtimes
+
+    word_input = input('word: ')
     color_code = input('color_code: ')
 
-    return word, color_code
-
-#(['c', 'r', 'a', 'n', 'e'], ['G', 'G', 'g', 'g', 'Y'])
-req_details = req_word_color_code()
-
-
-def most_common_word(word_input, color_code):
+    print(num_program_runtimes)
 
     green_indices = []
     yellow_indices = []
 
     grey_letters = []
+    yellow_letters = []
 
     for i in range(len(color_code)):
         if color_code[i] == "G":
@@ -30,32 +32,39 @@ def most_common_word(word_input, color_code):
             grey_letters.append(word_input[i])
 
         if color_code[i] == "Y":
+            yellow_letters.append(word_input[i])
             yellow_indices.append(i)
 
-    G_letter_words = []
-    Y_letter_words = []
-
     for word in wordle_answers:
-        match_count = 0
+        green_match_count = 0
 
-        
+
         for green_index in green_indices:
-            match_count += 1 if word[green_index] == word_input[green_index] else 0
-        
+            if word[green_index] != word_input[green_index]:
+                print(f"non_green_letter words = {word}")
+                wordle_answers.remove(word)
+
         if any(letter in grey_letters for letter in word):
-            match_count = 0
+            wordle_answers.remove(word)
         # abate atlas
         # for yellow letters they can't be in the same position so make match count 0
         for yellow_index in yellow_indices:
             if word[yellow_index] == word_input[yellow_index]:
-                match_count = 0
-                
-        if match_count == len(green_indices):
-            G_letter_words.append(word)
-    print(G_letter_words)
+                wordle_answers.remove(word)
+
+        for y_letters in yellow_letters:
+            if y_letters not in word:
+                wordle_answers.remove(word)
 
 
-print(most_common_word(req_details[0], req_details[1]))
+
+    print(most_probable_words)
+
+    num_program_runtimes += 1
+
+while True:
+    print(most_common_word())
+
 
 
 
