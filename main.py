eb3640cc9520b8fb53a0d28b93194691d762515c
Zafir <zafirhasan2006@gmail.com>
2022-09-01 16:09:35 +0400

    Add some logic for dealing with yellow letters
    The code now eliminates words that have the same letters as the word
    that is input in the yellow indices.

--- main.py
+++ main.py
@@ -17,22 +17,21 @@ req_details = req_word_color_code()
 
 def most_common_word(word_input, color_code):
 
-    G_letter_index = []
-    Y_letter_index = []
+    green_indices = []
+    yellow_indices = []
 
     grey_letters = []
 
     for i in range(len(color_code)):
         if color_code[i] == "G":
-            G_letter_index.append(i)
+            green_indices.append(i)
 
         if color_code[i] == "g":
             grey_letters.append(word_input[i])
 
         if color_code[i] == "Y":
-            Y_letter_index.append(i)
+            yellow_indices.append(i)
 
-    print(grey_letters, len(G_letter_index))
     G_letter_words = []
     Y_letter_words = []
 
@@ -40,13 +39,18 @@ def most_common_word(word_input, color_code):
         match_count = 0
 
         
-        for green_index in G_letter_index:
+        for green_index in green_indices:
             match_count += 1 if word[green_index] == word_input[green_index] else 0
         
         if any(letter in grey_letters for letter in word):
             match_count = 0
         # abate atlas
-        if match_count == len(G_letter_index):
+        # for yellow letters they can't be in the same position so make match count 0
+        for yellow_index in yellow_indices:
+            if word[yellow_index] == word_input[yellow_index]:
+                match_count = 0
+                
+        if match_count == len(green_indices):
             G_letter_words.append(word)
     print(G_letter_words)
 
