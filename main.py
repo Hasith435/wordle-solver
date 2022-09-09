a9d6718975792006ada5690fb128386595c4a3c3
Hasith435 <66883843+Hasith435@users.noreply.github.com>
2022-09-09 14:47:12 +0400

    Made the program use the previously generated word_list for the next prediction and adjusted the way of finding the most probable words a bit

--- main.py
+++ main.py
@@ -4,19 +4,27 @@ from words import get_wordle_answers, get_wordle_guesses
 wordle_answers = get_wordle_answers()
 wordle_guesses = get_wordle_guesses()
 
-num_program_runtimes = 0
+func_run_counter = 0
 word_database = 0
 
 most_probable_words = []
 
 
 def most_common_word():
-    global word_database, num_program_runtimes
+    global word_database, func_run_counter
+
+    buffer_list = []
+
+    if func_run_counter == 0:
+        word_database = wordle_answers
+
+    elif func_run_counter > 0:
+        word_database = most_probable_words
 
     word_input = input('word: ')
     color_code = input('color_code: ')
 
-    print(num_program_runtimes)
+    print(func_run_counter)
 
     green_indices = []
     yellow_indices = []
@@ -35,32 +43,41 @@ def most_common_word():
             yellow_letters.append(word_input[i])
             yellow_indices.append(i)
 
-    for word in wordle_answers:
+    for word in word_database:
         green_match_count = 0
 
 
         for green_index in green_indices:
-            if word[green_index] != word_input[green_index]:
-                print(f"non_green_letter words = {word}")
-                wordle_answers.remove(word)
+            if word[green_index] == word_input[green_index]:
+                green_match_count += 1
+
 
         if any(letter in grey_letters for letter in word):
-            wordle_answers.remove(word)
+            green_match_count = 0
+
         # abate atlas
         # for yellow letters they can't be in the same position so make match count 0
         for yellow_index in yellow_indices:
             if word[yellow_index] == word_input[yellow_index]:
-                wordle_answers.remove(word)
+                green_match_count = 0
+
 
         for y_letters in yellow_letters:
             if y_letters not in word:
-                wordle_answers.remove(word)
+                green_match_count = 0
 
+        if green_match_count == len(green_indices):
+            buffer_list.append(word)
 
+    print(f"buffer_list ={buffer_list}")
+    most_probable_words.clear()
+    for buffer_words in buffer_list:
+        most_probable_words.append(buffer_words)
+    buffer_list.clear()
 
     print(most_probable_words)
 
-    num_program_runtimes += 1
+    func_run_counter += 1
 
 while True:
     print(most_common_word())
